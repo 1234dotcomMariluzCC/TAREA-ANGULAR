9a2d43d4422401d4c28acaa36a094c7252845a08
Mariluz Cussi Cori <55479404+1234dotcomMariluzCC@users.noreply.github.com>
2024-06-07 21:40:21 -0400

    Add files via upload

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+
+export const routes: Routes = [
+    {path:'menu',component:MenuComponent},
+    {path:'body',component:BodyComponent},
+    {path:'footer',component:FooterComponent},
+];
